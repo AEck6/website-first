b789d18519dcbd48ad07c117a5a8f4c38b2d3284
AEck <Adam.eck26@gmail.com>
2022-03-11 20:08:08 -0600

    added html framework

---
+++ webpage.py
@@ -0,0 +1,11 @@
+index_frame = '''
+<html>
+    <div>
+        <body>
+    <div>
+        <h1>Sample Header</h1>
+</html>            
+'''
+file = open("index.html","w")
+file.write(index_frame)
+file.close()
